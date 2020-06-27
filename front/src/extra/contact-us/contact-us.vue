761de841465c91c0d7a149fab3eb2bf297e49e44
boltegg <boltegg@icloud.com>
2020-06-27 22:56:10 +0300

    [Feature] Display message about send status

--- front/src/extra/contact-us/contact-us.vue
+++ front/src/extra/contact-us/contact-us.vue
@@ -3,23 +3,21 @@
         <h2 class="content-title primary--text mb-4">Зв'яжіться з нами</h2>
         <h2 class="primary--text title font-weight-light text-uppercase mb-3">надіслати повідомлення</h2>
         <v-form ref="form" lazy-validation>
-            <v-text-field v-model="name" label="ім'я" box required></v-text-field>
-            <v-text-field v-model="email" label="Електронна адреса" box required></v-text-field>
-            <v-text-field v-model="phone" label="Телефон" box required></v-text-field>
-            <v-text-field v-model="message" label="Текст повідомлення" height="200px" box
+            <v-text-field v-model="name" label="ім'я" v-bind:readonly="answer!=null" box required></v-text-field>
+            <v-text-field v-model="email" label="Електронна адреса" v-bind:readonly="answer!=null" box
                           required></v-text-field>
-            <v-layout v-if="$vuetify.breakpoint.mdAndUp" justify-end>
-                <v-btn raised color="primary" class="ma-0" v-on:click="formSubmit">надіслати</v-btn>
+            <v-text-field v-model="phone" label="Телефон" v-bind:readonly="answer!=null" box required></v-text-field>
+            <v-text-field v-model="message" label="Текст повідомлення" v-bind:readonly="answer!=null" height="200px" box
+                          required></v-text-field>
+            <v-layout v-if="answer!=null" class="right">
+                <span>{{answer}}</span>
             </v-layout>
-            <v-layout v-else justify-center>
-                <v-btn raised color="primary" v-on:click="formSubmit">надіслати</v-btn>
+            <v-layout v-else justify-end>
+                <v-btn raised color="primary" class="ma-0" v-on:click="formSubmit">надіслати</v-btn>
             </v-layout>
         </v-form>
     </v-flex>
 </template>
-
-<!--<script src="./contact-us.ts"></script>-->
-
 
 <script>
     import axios from 'axios';
@@ -27,10 +25,11 @@
     export default {
         data() {
             return {
-                name: '',
-                email: '',
-                phone: '',
-                message: '',
+                name: null,
+                email: null,
+                phone: null,
+                message: null,
+                answer: null,
             };
         },
         methods: {
@@ -44,10 +43,12 @@
                     message: this.message
                 })
                     .then(function (response) {
+                        currentObj.answer = "Ваше повідомлення було надіслано!"
                         currentObj.output = response.data;
                     })
                     .catch(function (error) {
-                        currentObj.output = error;
+                        currentObj.answer = "Сталася помилка :( \n"
+                        console.log(error);
                     });
             }
         }
